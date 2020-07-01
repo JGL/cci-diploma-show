bac51f77bb9fe462eef6b0edadef5c0cc9447ead
Jessie Zhang <jessiezhang98@gmail.com>
2020-07-01 14:36:09 +0100

    updated mobile version buttons

--- public/about-page.js
+++ public/about-page.js
@@ -2,6 +2,12 @@ let about_div = document.getElementById("about-button");
 let about_page_bg = document.getElementById("about-page");
 
 function createAboutPage() {
+
+    if (mobile) {
+        about_div.style.top = "auto";
+        about_div.style.bottom = "0px";
+        about_div.style.left = "0px";
+    }
     let button = document.createElement("button");
 
     button.textContent = ">ABOUT";
